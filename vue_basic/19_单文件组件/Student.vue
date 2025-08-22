e569e93c62dc71adec8fffd0023bc3c01bbc6b3e
BillyNan_Lu <lunan96789@gmail.com>
2025-08-22 23:58:28 +0800

    add file 18-19 to vue_basic

---
+++ vue_basic/19_单文件组件/Student.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h2>学生姓名：{{ name }}</h2>
+    <h2>学生年龄：{{ age }}</h2>
+  </div>
+</template>
+
+<script>
+    export default {
+    name: "Student",
+    data() {
+        return {
+        name: "GeJingyi",
+        age: 19,
+        };
+    },
+    };
+</script>
+
+<style lang='' scoped>
+</style>
